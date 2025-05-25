fd294705bf27b94fd3b5b10ebd18fd4c82657423
Pablo Villar Morón <pablo.villar.moron@gmail.com>
2025-05-25 18:04:53 +0200

    adicion-cartas

--- src/paginas/DragQueenCard.jsx
+++ src/paginas/DragQueenCard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 
 function DragQueenCard() {
+
+  const params = useParams()
+  const [queen, setQueens] = useState(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/information/${params.queenId}`)
+      setQueens(response.data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  if (!queen) return <p>Loading...</p>
+
   return (
     <div>
-      
+      <div>
+      <h1>{queen.name}</h1>
+      <p>{queen.description}</p>
+      <img src={queen.imageUrl} alt={queen.name} />
+    </div>
     </div>
   )
 }
--- src/paginas/InformationPage.jsx
+++ src/paginas/InformationPage.jsx
@@ -1,19 +1,66 @@
-import "./InformationPage.css"
-import DragQueenCard from './DragQueenCard'
-import SeasonCard from './SeasonCard'
-import EpisodeCard from './EpisodeCard'
+import "./InformationPage.css";
+import { useParams } from "react-router-dom";
+import { useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function InformationPage() {
+
+  const params = useParams()
+  const [queens, setQueens] = useState([])
+
+  useEffect (() => {
+
+    axios.get(`${import.meta.env.VITE_SERVER_URL}/information`)
+    .then((response) => {
+      setQueens(response.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+
   return (
     <div id="serchbar">
       <input></input>
       <label></label>
-      <DragQueenCard />
-      <SeasonCard />
-      <EpisodeCard />
+      {queens &&
+        queens.map((eachQueen, i) => {
+          return (
+            <div key={i}>
+              <Link to={"/information/" + eachQueen.id}>
+                <div
+                  className="card m-2 p-2 text-center"
+                  style={{ width: "24rem", height: "18rem" }}
+                >
+                  <div className="card-body">
+                    <img
+                      src={eachQueen.image}
+                      style={{ height: "6rem" }}
+                      alt={"image of" + eachQueen.name}
+                    />
+                    <h5 className="card-title text-truncate mt-2">
+                      {eachQueen.name}
+                    </h5>
+                    <h6 className="card-subtitle mb-3 text-muted">
+                      <em>{eachQueen.season}</em>
+                    </h6>
+                    <p className="card-text">
+                      Season: {eachQueen.winner}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
 
+      {/* <DragQueenCard />
+      <SeasonCard />
+      <EpisodeCard /> */}
     </div>
-  )
+  );
 }
 
-export default InformationPage
+export default InformationPage;
